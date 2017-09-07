d85cc98dc0046cfd6119a7fd986225b62649af6a
CG-R8 <cgajare1@binghamton.edu>
2017-09-07 12:22:02 -0400

    Date format fixed

--- DS_Project_01/src/multiThreaded/Server_Response.java
+++ DS_Project_01/src/multiThreaded/Server_Response.java
@@ -4,19 +4,23 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 //import java.time.ZoneId;
 //import java.time.ZonedDateTime;
 import java.util.Date;
+import java.util.TimeZone;
 
 import javax.activation.MimetypesFileTypeMap;
 
 public class Server_Response {
 
-	public String success_reponse(String date, Date last_modified_date, long content_length, String content_type) {
+	public String success_reponse(String date, String last_modified_date_string, long content_length, String content_type) {
 		String Resp_headers = "HTTP/1.0 200 OK\r\n";
 		Resp_headers = Resp_headers + "Server: " + "Chetan's Server" + "\r\n";
 		Resp_headers = Resp_headers + "Date: " + date + "\r\n";
-		Resp_headers = Resp_headers + "Last-Modified: " + last_modified_date + "\r\n";
+		Resp_headers = Resp_headers + "Last-Modified: " + last_modified_date_string + "\r\n";
 		Resp_headers = Resp_headers + "Content-Length: " + content_length + "\r\n";
 		Resp_headers = Resp_headers + "Content-Type: " + content_type + "\r\n";
 		Resp_headers = Resp_headers + "\r\n";
@@ -38,20 +42,26 @@ public class Server_Response {
 		FileInputStream fis = null;
 		try {
 			System.out.println("File name ------------->>> "+Client_Request.URL);
-			Client_Request.URL = "www/"+Client_Request.URL;
+			Client_Request.URL = "www/"+Client_Request.URL;			// Appended www directory
 			File requestedfile = new File(Client_Request.URL);
+			String RFC_7231_format = "EEE, dd MMM yyyy HH:mm:ss zzz";
+			SimpleDateFormat sdf = new SimpleDateFormat(RFC_7231_format);
+			sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+			
 			// String date =
 			// java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME
 			// .format(ZonedDateTime.now(ZoneId.of("GMT")));
-			String date = "Temporary date";
+			Date today_date = new Date();
+			
+			String date = sdf.format(today_date);
 			long last_modified = requestedfile.lastModified();
 			Date last_modified_date = new Date(last_modified);
-			// last_modified_date=
-			// java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME
-			// .format(
+			
+			String last_modified_date_string = sdf.format(last_modified_date);
+			
 			String content_type = new MimetypesFileTypeMap().getContentType(requestedfile);
 			long content_length = requestedfile.length();
-			System.out.println("Last modified : " + last_modified_date + "\nContent Type: " + content_type);
+			System.out.println("Last modified : " + last_modified_date_string + "\nContent Type: " + content_type);
 			System.out.println("Content Length : " + content_length);
 			if (requestedfile.exists()) {
 
@@ -69,7 +79,7 @@ public class Server_Response {
 					// System.out.println("File Exist");
 					fis = new FileInputStream(requestedfile);
 					int ch = fis.read(bytes, 0, 2048);
-					byte[] b = success_reponse(date, last_modified_date, content_length, content_type).getBytes();
+					byte[] b = success_reponse(date, last_modified_date_string, content_length, content_type).getBytes();
 					// byte[] b = construct_http_header(200, 3).getBytes();
 					output.write(b);
 					while (ch != -1) {
@@ -80,7 +90,7 @@ public class Server_Response {
 
 			} else {
 				// file not found
-				// System.out.println("File DO NOT Exist");
+				System.out.println("File DO NOT Exist");
 				byte[] b = fail_response(date).getBytes();
 				output.write(b);
 
